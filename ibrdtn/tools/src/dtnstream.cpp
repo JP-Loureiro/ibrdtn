7d73bcddd61e2f4c39f3ecc3e7f5346d574aa120
JP-Loureiro <49435660+JP-Loureiro@users.noreply.github.com>
2020-11-03 13:49:41 +0000

    Update dtnstream.cpp

--- ibrdtn/tools/src/dtnstream.cpp
+++ ibrdtn/tools/src/dtnstream.cpp
@@ -190,7 +190,7 @@ int main(int argc, char *argv[])
 			if (_bundle_encryption) bs.base().set(dtn::data::PrimaryBlock::DTNSEC_REQUEST_ENCRYPT, true);
 			if (_bundle_signed) bs.base().set(dtn::data::PrimaryBlock::DTNSEC_REQUEST_SIGN, true);
 			if (_bundle_group) bs.base().set(dtn::data::PrimaryBlock::DESTINATION_IS_SINGLETON, false);
-			std::cout << "OI GATA"; 
+			std::cout << "OI GATA" << endl; 
 			std::ostream stream(&bs.rdbuf());
 			stream << std::cin.rdbuf() << std::flush;
 		}
