236013bc0a766c0b127b5817a8a39b39868386f7
JP-Loureiro <49435660+JP-Loureiro@users.noreply.github.com>
2020-12-16 11:28:43 +0000

    Update dtnstream.cpp

--- ibrdtn/tools/src/dtnstream.cpp
+++ ibrdtn/tools/src/dtnstream.cpp
@@ -207,8 +207,7 @@ int main(int argc, char *argv[])
 			std::vector<dtn::data::Number> seqNrs = aux.getSeqNrBuffer();
 			outfile.open("test.txt", std::ios::app);
 			for(dtn::data::Number n : seqNrs){
-				outfile << n;
-				outfile << "\n";
+				outfile << n << '\n';
 			}
 			outfile.close();
 		}
